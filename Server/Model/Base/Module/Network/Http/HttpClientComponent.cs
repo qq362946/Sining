dd0d1eacfcef5dd8f2bad07dd1bb7e760e8639b9
qq362946 <362946@qq.com>
2020-03-26 15:12:00 +0800

    重新设计了HttpClientComponent的逻辑代码。

--- Server/Model/Base/Module/Network/Http/HttpClientComponent.cs
+++ Server/Model/Base/Module/Network/Http/HttpClientComponent.cs
@@ -1,11 +1,7 @@
 using System;
-using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Text;
-using Server.Network;
 using Sining.Event;
-using Sining.Module;
 using Sining.Tools;
 
 namespace Sining.Network
@@ -22,63 +18,44 @@ namespace Sining.Network
     public class HttpClientComponent : Component
     {
         public static HttpClientComponent Instance { get; private set; }
-        private MessagePacker _messagePacker;
         private readonly HttpClient _client = new HttpClient();
-
         public void Awake()
         {
-            _messagePacker = AddComponent<JsonMessagePacker>();
             Instance = this;
         }
-
-        public void Send<T>(T t, string url) where T : IMessage
+        public async STask<string> CallNotDeserialize(string url, HttpContent content)
         {
-            try
-            {
-                SendAsync(url, t).Coroutine();
-            }
-            catch (Exception e)
-            {
-                Log.Error(e);
-            }
-        }
-
-        private async SVoid SendAsync<T>(string url, T t) where T : IMessage
-        {
-            var content = new StringContent(t.Serialize(), Encoding.UTF8);
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            var response = await _client.PostAsync($"{url}{NetworkProtocolManagement.Instance.GetRawUrl(typeof(T))}",
-                content);
+            var response = await _client.PostAsync(url, content);
 
             if (response.StatusCode != HttpStatusCode.OK)
             {
-                throw new Exception($"Unable to connect to server url {url} HttpStatusCode:{response.StatusCode}");
+                throw new Exception(
+                    $"Unable to connect to server url {(object) url} HttpStatusCode:{(object) response.StatusCode}");
             }
+
+            return await response.Content.ReadAsStringAsync();
         }
-
-        public async STask<TResponse> Call<TResponse>(IRequest request, string url) where TResponse : IResponse
+        public async STask<T> Call<T>(string url, HttpContent content)
         {
-            var content = new StringContent(request.Serialize(), Encoding.UTF8);
-            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
-            var response =
-                await _client.PostAsync($"{url}{NetworkProtocolManagement.Instance.GetRawUrl(request.GetType())}",
-                    content);
-
+            return await Deserialize<T>(url, await _client.PostAsync(url, content));
+        }
+        public async STask<T> Call<T>(string url, HttpMethod method)
+        {
+            return await Deserialize<T>(url, await _client.SendAsync(new HttpRequestMessage(method, url)));
+        }
+        private async STask<T> Deserialize<T>(string url, HttpResponseMessage response)
+        {
             if (response.StatusCode != HttpStatusCode.OK)
             {
-                throw new Exception($"Unable to connect to server url {url} HttpStatusCode:{response.StatusCode}");
+                throw new Exception(
+                    $"Unable to connect to server url {(object) url} HttpStatusCode:{(object) response.StatusCode}");
             }
-
-            var json = await response.Content.ReadAsStringAsync();
-
-            return _messagePacker.DeserializeFrom<TResponse>(json);
+       
+            return (await response.Content.ReadAsStringAsync()).Deserialize<T>();
         }
-
         public override void Dispose()
         {
             Instance = null;
-            _messagePacker = null;
-
             base.Dispose();
         }
     }
